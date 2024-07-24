768e60b449795febf2c20f5f5f348863a17fccb2
Bishal Chansi <chansibishal55@gmail.com>
2024-07-24 16:15:46 +0545

    crud complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,151 +8,166 @@ const App = () => {
   const authCode = import.meta.env.VITE_AUTHCODE;
 
   const [showData, setShowData] = useState([]);
-  const [name, setName] = useState("");
-  const [position, setPosition] = useState("");
-  const [email, setEmail] = useState("");
-  const [comName, setComName] = useState("");
-  const [comment, setComment] = useState("");
-  const [user, setUser] = useState("");
+  const [editData, setEditData] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    position: "",
+    email: "",
+    comName: "",
+    comment: "",
+    image: "",
+    id: "",
+  });
   const [flag, setFlag] = useState(true);
-  const [image, setImage] = useState("");
-  const [id, setId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     handleShowPost();
   }, []);
 
-  const handleShowPost = () => {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        signature: "p0m76",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        UserID: "-1",
-        Flag: "S",
-        AuthCode: `${authCode}`,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setShowData(data.Values));
-  };
-
-  const handleEdit = (id) => {
-    // console.log(editData)
-    const filteredData = showData.filter((data) => data.TestimonialID === id);
-    // console.log(filteredData);
-    setName(filteredData[0].FullName);
-    setPosition(filteredData[0].Position);
-    setEmail(filteredData[0].Email);
-    setComName(filteredData[0].ComName);
-    setComment(filteredData[0].Comment);
-    setId(filteredData[0].TestimonialID);
-    setFlag(false);
-  };
-
-  const handleUpdate = async () => {
-    if(user === ''){
-      toast.error("Enter your User ID");
-      return;
-    }
-    const api = await fetch(url, {
-      method: "POST",
-      headers: {
-        signature: "p0m76",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        UserID: user,
-        Flag: "U",
-        TestimonialID: `${id}`,
-        FullName: name,
-        Position: position,
-        Email: email,
-        UserImage: image,
-        ComName: comName,
-        Comment: comment,
-        AuthCode: authCode,
-      }),
-    });
-    const apiData = await api.json();
-    handleShowPost();
-    handleClear();
-    toast.success("Updated Successfully");
-  };
-  const handleAdd = async () => {
-    if(user === ''){
-      toast.error("Enter your User ID");
-      return;
-    }
-    const api = await fetch(url, {
-      method: "POST",
-      headers: {
-        signature: "p0m76",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        UserID: user,
-        Flag: "I",
-        FullName: name,
-        Position: position,
-        Email: email,
-        ComName: comName,
-        Comment: comment,
-        UserImage: image,
-        AuthCode: authCode,
-      }),
-    });
-    const apiData = await api.json();
-    // console.log(apiData)
-    handleShowPost();
-    handleClear();
-    toast.success("Successfully Added");
-  };
-  const handleDelete = async (id) => {
-    const userId = prompt("Enter your User Id to delete")
-    if(userId === null){
-      toast.error("No User ID entered");
-      return;
-    }
-    if(userId <= 0){
-      toast.error("Incorrect User ID");
-      return;
-    }
-  
-    const filteredData = showData.filter((data) => data.TestimonialID == id);
-    // setUser()
-    if(window.confirm("Are you sure you want to delete this")){
-      const api = await fetch(url, {
+  const handleShowPost = async () => {
+    try {
+      const response = await fetch(url, {
         method: "POST",
         headers: {
           signature: "p0m76",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          UserID: userId,
-          Flag: "R",
-          TestimonialID: `${filteredData[0].TestimonialID}`,
+          UserID: "-1",
+          Flag: "S",
           AuthCode: `${authCode}`,
         }),
       });
-      const apiData = await api.json();
-      handleShowPost();
-      toast.success("Successfully Deleted");
+      const data = await response.json();
+      setEditData(data.Values);
+      setShowData(data.Values);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to fetch data from API");
     }
   };
-  const handleClear = () => {
-    setName("");
-    setPosition("");
-    setEmail("");
-    setComName("");
-    setComment("");
-    setId("");
-    setImage("");
-    setUser("");
-    setFlag(true);
+
+  const handleEdit = (id) => {
+    const filteredData = editData.find((data) => data.TestimonialID === id);
+    setFormData({
+      name: filteredData.FullName,
+      position: filteredData.Position,
+      email: filteredData.Email,
+      comName: filteredData.ComName,
+      comment: filteredData.Comment,
+      id: filteredData.TestimonialID,
+      image:
+        filteredData.UserImage === null
+          ? (filteredData.UserImage = "")
+          : filteredData.UserImage,
+    });
+    setFlag(false);
+    setIsVisible(true);
   };
+
+  const handleSave = async (flag) => {
+    const { name, position, email, comName, comment, image, id } = formData;
+    if (!userId) {
+      toast.error("Enter your User ID");
+      return;
+    }
+    const requestBody = {
+      UserID: userId,
+      Flag: flag === "I" ? "I" : "U",
+      FullName: name,
+      Position: position,
+      Email: email,
+      ComName: comName,
+      Comment: comment,
+      UserImage: image,
+      AuthCode: authCode,
+    };
+    if (flag === "U") requestBody.TestimonialID = `${id}`;
+
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          signature: "p0m76",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      await res.json();
+      handleShowPost();
+      handleClear();
+      toast.success(`Successfully ${flag === "I" ? "Added" : "Updated"}`);
+    } catch (error) {
+      toast.error("Failed to save data");
+    }
+    setIsVisible(false);
+  };
+
+  const handleDelete = async (id) => {
+    const userId = prompt("Enter your User Id to delete");
+    if (userId === null) {
+      toast.error("No User ID entered");
+      return;
+    }
+    if (userId <= 0) {
+      toast.error("Incorrect User ID");
+      return;
+    }
+
+    const filteredData = editData.find((data) => data.TestimonialID == id);
+
+    if (window.confirm("Are you sure you want to delete this")) {
+      try {
+        const api = await fetch(url, {
+          method: "POST",
+          headers: {
+            signature: "p0m76",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            UserID: userId,
+            Flag: "R",
+            TestimonialID: `${filteredData.TestimonialID}`,
+            AuthCode: authCode,
+          }),
+        });
+        await api.json();
+        handleShowPost();
+        toast.success("Successfully Deleted");
+      } catch (error) {
+        toast.error("Failed to delete data");
+      }
+    }
+  };
+
+  const handleClear = () => {
+    setFormData({
+      name: "",
+      position: "",
+      email: "",
+      comName: "",
+      comment: "",
+      image: "",
+      id: "",
+    });
+    setFlag(true);
+    setUserId("");
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSearch = (e) => {
+    const filteredData = editData.filter((item) =>
+      item.FullName.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setShowData(filteredData);
+  };
+
   const columns = [
     {
       name: "ID",
@@ -170,7 +185,6 @@ const App = () => {
       name: "Email Address",
       selector: (row) => row.Email,
     },
-
     {
       name: "Name",
       selector: (row) => row.ComName,
@@ -188,7 +202,7 @@ const App = () => {
       cell: (row) => (
         <div className="action">
           <button className="btn" onClick={() => handleEdit(row.TestimonialID)}>
-            Edit
+            Update
           </button>
           <button
             className="btn delete"
@@ -203,89 +217,97 @@ const App = () => {
 
   return (
     <div className="container">
-      <div className="inputField">
-        <div className="input">
-          <label htmlFor="id">User_ID:</label>
-          <input
-            type="text"
-            id="id"
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-          />
-        </div>
-        <div className="input">
-          <label htmlFor="name">
-            Full_Name:
+      {isVisible && (
+        <div className="inputField">
+          <div className="input">
+            <label htmlFor="Userid">User_ID:</label>
+            <input
+              type="text"
+              id="Userid"
+              value={userId}
+              onChange={(e) => setUserId(e.target.value)}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="name">Full_Name:</label>
             <input
               type="text"
               id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={formData.name}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="position">
-            Position:
+          </div>
+          <div className="input">
+            <label htmlFor="position">Position:</label>
             <input
               type="text"
               id="position"
-              value={position}
-              onChange={(e) => setPosition(e.target.value)}
+              value={formData.position}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="email">
-            E-mail:
+          </div>
+          <div className="input">
+            <label htmlFor="email">E-mail:</label>
             <input
               type="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={formData.email}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="Comname">
-            Company_Name:
+          </div>
+          <div className="input">
+            <label htmlFor="comName">Company_Name:</label>
             <input
               type="text"
-              id="Comname"
-              value={comName}
-              onChange={(e) => setComName(e.target.value)}
+              id="comName"
+              value={formData.comName}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="comment">
-            Comment:
+          </div>
+          <div className="input">
+            <label htmlFor="comment">Comment:</label>
             <input
               type="text"
               id="comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
+              value={formData.comment}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-        <div className="input image">
-          <label htmlFor="image">
-            Image:
-            <input type="text" id="image" />
-          </label>
-        </div>
-        <div className="input clear">
-          {flag ? (
-            <button style={{ backgroundColor: "blue" }} onClick={handleAdd}>
-              Add
+          </div>
+          <div className="input image">
+            <label htmlFor="image">Image:</label>
+            <input
+              type="text"
+              id="image"
+              value={formData.image}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="input clear">
+            <button
+              style={{ backgroundColor: flag ? "blue" : "green" }}
+              onClick={() => handleSave(flag ? "I" : "U")}
+            >
+              {flag ? "Add" : "Update"}
             </button>
-          ) : (
-            <button style={{ backgroundColor: "green" }} onClick={handleUpdate}>
-              Update
-            </button>
-          )}
-          <button onClick={handleClear}>Clear</button>
+            <button onClick={handleClear}>Clear</button>
+            <button onClick={() => setIsVisible(false)}>Back</button>
+          </div>
         </div>
+      )}
+      <div className="add">
+        <button
+          style={{ backgroundColor: "blue" }}
+          onClick={() => setIsVisible(true)}
+        >
+          Add New Data
+        </button>
+      </div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search here..."
+          onChange={handleSearch}
+        />
       </div>
       <DataTable
         columns={columns}
